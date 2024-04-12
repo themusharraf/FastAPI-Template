93cd1c72dcf989ee72d317ce529d562476a6dfc7
msi <meibrohimov.@gmail.com>
2024-04-12 23:01:42 +0500

    add new file 👁

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,3 +11,7 @@ colors = [{"name": "red", "type": "HTML"}, {"name": "Blue", "type": "Python"}]
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("main.html", {"request": request, "user": "All nc", "colors": colors})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000,)
